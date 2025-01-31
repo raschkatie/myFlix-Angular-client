8a4916ae4eb98aec38013d4eff1b06ecf52f1b6d
raschkatie <raschkatie@gmail.com>
2025-01-31 12:06:53 -0500

    Added Loading functionality during login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -13,31 +13,37 @@ import { Router } from '@angular/router';
 export class UserLoginFormComponent implements OnInit {
 
   @Input() userData = { Username: '', Password: '' };
+  isLoading = false;
 
-constructor(
-    public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar,
-    public router: Router
-  ) { }
+  constructor(
+      public fetchApiData: FetchApiDataService,
+      public dialogRef: MatDialogRef<UserLoginFormComponent>,
+      public snackBar: MatSnackBar,
+      public router: Router
+    ) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
 
-loginUser(): void {
+  loginUser(): void {
+    this.isLoading = true;
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
       localStorage.setItem('user', JSON.stringify(result.user));
       localStorage.setItem('token', result.token);
-     this.dialogRef.close();
-     this.snackBar.open('Log In Success!', 'OK', {
+      this.dialogRef.close();
+      this.snackBar.open('Log In Success!', 'OK', {
         duration: 2000
-     });
-     this.router.navigate(['movies']);
-    }, (result) => {
+      });
+      this.router.navigate(['movies']);
+    }, 
+    (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
+    },
+    () => {
+      this.isLoading = false;
     });
   }
 
-  }
+}
