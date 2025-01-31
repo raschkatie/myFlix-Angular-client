a99e2c1f66080c96779a9feb6a2db83f5234b841
raschkatie <raschkatie@gmail.com>
2025-01-31 15:20:55 -0500

    Removed console.logs during login

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -17,14 +17,12 @@ export class FetchApiDataService {
   }
 
   public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http.post(`${apiUrl}users`, userDetails).pipe(
       catchError(this.handleError)
     );
   }
 
   public userLogin(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http.post(`${apiUrl}login`, userDetails).pipe(
       catchError(this.handleError)
     );
