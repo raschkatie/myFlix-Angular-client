a0681381f45189d217f3d52bf464f95fb3e268e2
raschkatie <raschkatie@gmail.com>
2025-02-04 14:23:49 -0500

    TypeDoc documentation

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -3,6 +3,14 @@ import { MatDialogRef } from "@angular/material/dialog";
 import { FetchApiDataService } from "../fetch-api-data.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
+/**
+ * This is the component that allows the user to register a new account.
+ * 
+ * UserRegistrationFormComponent opens a dialog window with a registration form
+ * that the user can fill out with their username, password, email address,
+ * and birthday. The dialog is closed after the account is successfully created.
+ */
+
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
@@ -21,11 +29,14 @@ export class UserRegistrationFormComponent implements OnInit {
       
     }
 
-    // this is the function responsible for sending the form inputs to the backend
+    /**
+     * Takes the user's new information and creates an account.
+     * 
+     * @returns {void} - does not return a value
+     */
     registerUser(): void {
       this.fetchApiData.userRegistration(this.userData).subscribe((result: string) => {
-        // logic for a successful user registration goes here later
-        this.dialogRef.close(); // this will close the modal on success
+        this.dialogRef.close();
         this.snackBar.open('Account successfully created! Please log in.', 'OK', {
           duration: 2000
         });
